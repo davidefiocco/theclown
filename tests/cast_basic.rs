503d7e509fd192a45861fe35a0b55fed09227fab
davide.fiocco <4547987+davidefiocco@users.noreply.github.com>
2026-02-25 23:35:28 +0100

    Support casting between numerical formats

---
+++ tests/cast_basic.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let x: i64 = 42;
+    let y: f64 = x as f64;
+    println!("{}", y);
+    let z: f64 = 3.9;
+    let w: i64 = z as i64;
+    println!("{}", w);
+}
---
+++ tests/cast_unsupported.rs
@@ -0,0 +1,5 @@
+fn main() {
+    let x: i64 = 65;
+    let c = x as char;
+    println!("{}", c);
+}
